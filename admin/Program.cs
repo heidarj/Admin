3856e70246a758d52dc3d1fd8418ba2bda61d3c9
heidarj <heidarj@hraunteigur5.com>
2015-06-13 20:32:21 +0000

    Admin Project
    Allows you to open an elevated command in the present working directory
    of the command promt from which it was executed. For best results add
    "admin.exe" to a folder which has been added to the system PATH
    variable, so it can be called from anywhere

---
+++ admin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string path = Directory.GetCurrentDirectory();
+                var psi = new ProcessStartInfo("cmd.exe");
+                psi.Verb = "runas";
+                psi.UseShellExecute = false;
+                psi.Arguments = string.Format("/d {0} ", path);
+                Process.Start(psi);
+        }
+    }
+}
